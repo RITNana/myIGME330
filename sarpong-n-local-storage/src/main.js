f17df22687b3820bf88a75b9f1fcdfa96e8f24f0
Nana Sarpong <ns8363@g.rit.edu>
2024-04-21 23:10:37 -0400

    LocalStorage

--- sarpong-n-local-storage/src/main.js
+++ sarpong-n-local-storage/src/main.js
@@ -1,38 +1,63 @@
 import * as storage from "./storage.js"
-let items = ["???!!!"];
+let items = ["???"]
 
+// create name for key
+let key = "items";
+
+//  write items
+const writeItems = () => {
+  storage.writeToLocalStorage(key, items)
+}
 
 // I. declare and implement showItems()
 // - this will show the contents of the items array in the <ol>
 const showItems = () => {
   let html = "";
   // loop though items and stick each array element into an <li>
-  for(let i of items){
-    btnList += storage.writeToLocalStorage(i, i).map(i => `<li>${i}</li>`)
-  }
-
-  document.querySelector("#btn-add").innerHTML = html
-  
+  html += `<ol> ${items.map(i => `<li>${i}</li>`).join("")}</ol>`
+  document.querySelector(".ml-4").innerHTML = html;
   // use array.map()!
   // update the innerHTML of the <ol> already on the page
 };
 
+
 // II. declare and implement addItem(str)
 // - this will add `str` to the `items` array (so long as `str` is length greater than 0)
 const addItem = str => {
-
+  if(str.length > 0){
+    items.push(str)
+  }
 };
-
 
 // Also:
 // - call `addItem()`` when the button is clicked, and also clear out the <input>
 // - and be sure to update .localStorage by calling `writeToLocalStorage("items",items)`
+document.querySelector("#btn-add").addEventListener("click", () => {
+  addItem(document.querySelector("#thing-text").value)
+  document.querySelector("#thing-text").value = "";
+  writeItems();
+  showItems();
+})
 
 // When the page loads:
 // - load in the `items` array from storage.js and display the current items
 // you might want to double-check that you loaded an array ...
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/isArray
 // ... and if you didn't, set `items` to an empty array
+document.addEventListener("DOMContentLoaded", () => {
+  // Load the items array from local storage
+  items = storage.readFromLocalStorage(key);
+  
+  // Check if the loaded value is an array, and if not, set items to an empty array
+  if (!Array.isArray(items)) {
+    items = [];
+  }
+  showItems();
+});
 
 // Got it working? 
 // - Add a "Clear List" button that empties the items array
+document.querySelector("#btn-clear").addEventListener("click", () => {
+  items = [];
+  writeItems();
+})
--- sarpong-n-local-storage/src/storage.js
+++ sarpong-n-local-storage/src/storage.js
@@ -1,6 +1,6 @@
 // a private (to this module) unique name to store the app data under
 // if you put this on banjo, change `abc1234` to your banjo account name
-const storeName = "ns8363-list-app";
+const storeName = "Nana's Inputted Names :)";
 
 // a private (to this module) helper function
 // it will load in a string from localStorage and convert it to a JSON object
